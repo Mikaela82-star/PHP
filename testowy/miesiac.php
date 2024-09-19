82eeadca596c4a8f1541249ceacce91c7b080fe8
Mikaela82-star <sierakoliwia@gmail.com>
2024-09-19 17:19:03 +0200

    mess

--- testowy/miesiac.php
+++ testowy/miesiac.php
@@ -7,5 +7,46 @@
 </head>
 <body>
     
+<?php
+$mies = rand(1,12);
+
+switch($mies){
+    case 1:
+    case 2:
+    case 3:
+      $typ="I kwartał";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      $typ="II kwartał";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      $typ="III kwartał";
+      break;
+    case 10:
+    case 11:
+    case 12:
+      $typ="IV kwartał";
+      break;
+    default:
+      $typ="Błędny numer miesiąca";
+      break;
+}
+
+?>
+
+<hr>
+<h1><?=$mies?></h1>
+<hr>
+<h3><?=$typ?></h3>
+<hr>
+
+
+
+
+
 </body>
 </html>
