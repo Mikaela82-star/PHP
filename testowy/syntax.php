3b81121399226feb6ab62db1ea5c1c448e1d89e4
Mikaela82-star <sierakoliwia@gmail.com>
2024-09-19 16:18:18 +0200

    mesa

---
+++ testowy/syntax.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+
+<head>
+
+  <meta charset="UTF-8">
+  <title>Formularz rejestracyjny</title>
+
+</head>
+
+<body>
+
+<?php
+
+echo "Tu funkcja echo";
+echo "Tu"."funkcja"."echo";
+echo ('Tu funkcja echo');
+echo 'Tu'.'funkcja'.'echo';
+
+print('Zglasza'.'sie'.'funkcja'.'print');
+print "Zglasza"."sie"."funkcja"."print";
+
+//echo "nijako";
+#echo "nijako";
+
+/*echo "nijako";
+  echo "jablko";
+*/
+
+?>
+
+<?= "Cole jest czarny" ?>
+<?= "Jump up,"."kick back,"."whip around"."and spin" ?>
+
+
+
+</body>
+</html>
