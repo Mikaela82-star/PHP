9bac28acf45086c6b62ab6c9c19c8286f3333970
Mikaela82-star <sierakoliwia@gmail.com>
2024-09-20 13:56:10 +0200

    mess

---
+++ testowy/wiersze_w_petli_wh.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    td {
+     
+    }
+    </style>
+</head>
+<body>
+
+<table>
+<?php
+
+$i=1;
+while($i<6){
+
+
+  echo "<tr><td>"."To jest wiersz numer $i"."</td></tr>";
+  $i++;
+
+}
+
+?>
+</table>
+
+
+</body>
+</html>
