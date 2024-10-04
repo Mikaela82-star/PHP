2523c191e4ce0c704df9e9d13502697288b25afe
Mikaela82-star <sierakoliwia@gmail.com>
2024-10-04 13:51:12 +0200

    mess

---
+++ testowy/petlee/sto_el_listy.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<ol type="I">
+<?php
+
+for($i=1;$i<101;$i++){
+ echo '<li style="margin-left: 40px">To jest '.$i.'. element listy</li>';
+} 
+
+?>
+
+</ol>
+
+
+</body>
+</html>
