80f627834495e3ae86e491f5e52e77ffcecb3035
Mikaela82-star <sierakoliwia@gmail.com>
2024-11-08 13:53:26 +0100

    mess

--- testowy/dzial2/artymetyka.php
+++ testowy/dzial2/artymetyka.php
@@ -2,22 +2,26 @@
 
 class Arytmetyka {
 
-    public function __construct(float $a, float $b){
-        $this->liczbaA = $a;
-        $this->liczbaB = $b;
+    public ?float $a = null;
+    public ?float $b = null;
+
+    public function __construct(float $a1, float $b1){
+        $this->a = $a1;
+        $this->b = $b1;
     }
 
     public function suma(){
-        return $this->a + $this->$b;
+        return $this->a + $this->b;
     }
     public function roznica(){
         return $this->a - $this->b;
     }
     public function iloczyn(){
-        if($this->a==0 || $this>b==0){
+        if($this->a==0 || $this->b==0){
             return null;
+        }else{
+            return $this->a * $this->b;
         }
-        return $this->a * $this->b;
     }
     public function iloraz(){
         if($this->a==0 || $this->b==0){
